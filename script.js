a8709920985ed9124b57c9b95c5c5c03109fbedf
seanenriquez <seanenriquez@gmail.com>
2020-09-23 02:40:27 -0400

    fetch

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //console.log(settings)
 
 fetch('https://young-crag-72283.herokuapp.com/restaurants')
-  .then(response => response.json())
+  .then(response => console.log(response))
   .then(data => console.log(data));
 
 (function waitFor() {
